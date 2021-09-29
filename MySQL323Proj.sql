b0c6024bd93af917e18e8aef76a931e5f9761c46
TimonAugustyn <71490310+TimonAugustyn@users.noreply.github.com>
2021-09-29 18:15:14 +0200

    Add files via upload
    Added the account table I will be using for this project

--- MySQL323Proj.sql
+++ MySQL323Proj.sql
@@ -2,13 +2,14 @@ set echo on
 
 DROP TABLE DEMO_ACCOUNT_TRANSACTION CASCADE CONSTRAINTS;
 DROP TABLE DEMO_ACCOUNT_TYPE CASCADE CONSTRAINTS;
+DROP TABLE ACCOUNT CASCADE CONSTRAINTS;
 DROP SEQUENCE VIT_RSA_GENERIC_SEQ;
 
 create table DEMO_ACCOUNT_TYPE (
-    ACCOUNT_TYPE_ID number(15) not null,
-    MNEMONIC varchar2(50) not null,
-    ACCOUNT_TYPE_NAME varchar2(50) not null,
-    CREATION_DATE DATE not null,
+    ACCOUNT_TYPE_ID number(15),
+    MNEMONIC varchar2(50),
+    ACCOUNT_TYPE_NAME varchar2(50),
+    CREATION_DATE DATE,
     constraint PK_TYPE primary key (ACCOUNT_TYPE_ID)
 );
 
@@ -20,15 +21,24 @@ maxvalue 1000
 cycle;
 
 create table DEMO_ACCOUNT_TRANSACTION (
-    TX_ID number(15) not null,
-    ACCOUNT_TYPE_ID number(15) not null,
-    MEMBER_ID number(15) not null,
-    AMOUNT number(4) not null,
-    TX_DATE DATE not null,
+    TX_ID number(15),
+    ACCOUNT_TYPE_ID number(15),
+    MEMBER_ID number(15),
+    AMOUNT number(4),
+    TX_DATE DATE,
     constraint PK_TRAN primary key (TX_ID),
     constraint fk_ACCOUNT_TYPE_ID foreign key (ACCOUNT_TYPE_ID)
     references DEMO_ACCOUNT_TYPE (ACCOUNT_TYPE_ID) on delete set NULL
 );
+
+create table ACCOUNT(
+    MEMBER_ID number(15),
+    FNAME varchar2(15),
+    LNAME varchar2(15),
+    MILES number(15),
+    constraint PK_ACCOUNT primary key (MEMBER_ID)
+);
+
 
 COMMIT;
 
